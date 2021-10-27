8b66cfffec6d3255fca376f3a9af1c5df7d9fe16
eder <ederoliveirabarreto@gmail.com>
2021-10-27 16:40:37 -0300

    Add admin user access to actions throughout application

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,9 @@ class UsersController < ApplicationController
 
   def destroy
     @user.destroy
-    session[:user_id] = nil
+    if current_user == @user
+      session[:user_id] = nil
+    end 
     flash[:notice] = "Account and all associated articles successfully deleted."
     redirect_to articles_path
   end
@@ -56,8 +58,8 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-		if current_user != @user
-			flash[:alert] = "You can only edit your own profile"
+		if current_user != @user && !current_user.admin?
+			flash[:alert] = "You can only edit or delete your own account."
 			redirect_to @user
 		end
 	end 
